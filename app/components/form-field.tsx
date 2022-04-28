eccb6a2e90d3919033d1f08d72fbbe7847726d15
Michael Frieze <michaelfrieze@icloud.com>
2022-04-28 17:28:45 -0400

    created form-field component

---
+++ app/components/form-field.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+
+interface FormFieldProps {
+  htmlFor: string;
+  label: string;
+  type?: string;
+  value: any;
+  onChange?: (...args: any) => any;
+  error?: string;
+}
+
+export function FormField({
+  htmlFor,
+  label,
+  type = 'text',
+  value,
+  onChange = () => {},
+  error = '',
+}: FormFieldProps) {
+  const [errorText, setErrorText] = useState(error);
+
+  useEffect(() => {
+    setErrorText(error);
+  }, [error]);
+
+  return (
+    <>
+      <label htmlFor={htmlFor} className="text-blue-600 font-semibold">
+        {label}
+      </label>
+      <input
+        onChange={(e) => {
+          onChange(e);
+          setErrorText('');
+        }}
+        type={type}
+        id={htmlFor}
+        name={htmlFor}
+        className="w-full p-2 rounded-xl my-2"
+        value={value}
+      />
+      <div className="text-xs font-semibold text-center tracking-wide text-red-500 w-full">
+        {errorText || ''}
+      </div>
+    </>
+  );
+}
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,6 +1,26 @@
 import { Layout } from '~/components/layout';
+import { FormField } from '~/components/form-field';
+import { useState } from 'react';
+
+// 11:34
+// https://www.youtube.com/watch?v=vR33ZRJekHk
 
 export default function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    field: string
+  ) => {
+    setFormData((form) => ({
+      ...form,
+      [field]: event.target.value,
+    }));
+  };
+
   return (
     <Layout>
       <div className="h-full flex justify-center items-center flex-col gap-y-4">
@@ -11,25 +31,22 @@ export default function Login() {
           Log In To Give Some Praise!
         </p>
 
+        {JSON.stringify(formData)}
+
         <form className="rounded-2xl bg-gray-200 p-6 w-96">
-          <label htmlFor="email" className="text-blue-600 font-semibold">
-            Email
-          </label>
-          <input
-            type="text"
-            id="email"
-            name="email"
-            className="w-full p-2 rounded-xl my-2"
+          <FormField
+            htmlFor="email"
+            label="Email"
+            value={formData.email}
+            onChange={(e) => handleInputChange(e, 'email')}
           />
 
-          <label htmlFor="password" className="text-blue-600 font-semibold">
-            Password
-          </label>
-          <input
+          <FormField
+            htmlFor="password"
+            label="Password"
             type="password"
-            id="password"
-            name="password"
-            className="w-full p-2 rounded-xl my-2"
+            value={formData.password}
+            onChange={(e) => handleInputChange(e, 'password')}
           />
 
           <div className="w-full text-center">
