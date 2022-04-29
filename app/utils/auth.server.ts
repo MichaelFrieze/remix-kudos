a87709f89e36a020fd3e53130e8880540d6a00a2
Michael Frieze <michaelfrieze@icloud.com>
2022-04-29 15:04:55 -0400

    added more user functions to auth util

--- app/utils/auth.server.ts
+++ app/utils/auth.server.ts
@@ -84,3 +84,36 @@ export async function requireUserId(
 function getUserSession(request: Request) {
   return storage.getSession(request.headers.get('Cookie'));
 }
+
+async function getUserId(request: Request) {
+  const session = await getUserSession(request);
+  const userId = session.get('userId');
+  if (!userId || typeof userId !== 'string') return null;
+  return userId;
+}
+
+export async function getUser(request: Request) {
+  const userId = await getUserId(request);
+  if (typeof userId !== 'string') {
+    return null;
+  }
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: { id: true, email: true, profile: true },
+    });
+    return user;
+  } catch {
+    throw logout(request);
+  }
+}
+
+export async function logout(request: Request) {
+  const session = await getUserSession(request);
+  return redirect('/login', {
+    headers: {
+      'Set-Cookie': await storage.destroySession(session),
+    },
+  });
+}
