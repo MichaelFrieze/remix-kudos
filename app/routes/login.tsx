6a435c87846a92f7a19566112d73ce8e130e404e
Michael Frieze <michaelfrieze@icloud.com>
2022-04-28 20:50:06 -0400

    added buttons to the login form that do different things depending on if action is log in or sign up

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -2,13 +2,16 @@ import { Layout } from '~/components/layout';
 import { FormField } from '~/components/form-field';
 import { useState } from 'react';
 
-// 11:34
+// 24:40
 // https://www.youtube.com/watch?v=vR33ZRJekHk
 
 export default function Login() {
+  const [action, setAction] = useState('login');
   const [formData, setFormData] = useState({
     email: '',
     password: '',
+    firstName: '',
+    lastName: '',
   });
 
   const handleInputChange = (
@@ -24,14 +27,21 @@ export default function Login() {
   return (
     <Layout>
       <div className="h-full flex justify-center items-center flex-col gap-y-4">
+        <button
+          onClick={() => setAction(action === 'login' ? 'register' : 'login')}
+          className="absolute top-8 right-8 rounded-xl bg-yellow-300 font-semibold text-blue-600 px-3 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
+        >
+          {action === 'login' ? 'Sign Up' : 'Login'}
+        </button>
+
         <h2 className="text-5xl font-extrabold text-yellow-300">
           Welcome to Kudos!
         </h2>
         <p className="font-semibold text-slate-300">
-          Log In To Give Some Praise!
+          {action === 'login'
+            ? 'Log In To Give Some Praise!'
+            : 'Sign Up To Get Started!'}
         </p>
-
-        {JSON.stringify(formData)}
 
         <form className="rounded-2xl bg-gray-200 p-6 w-96">
           <FormField
@@ -49,12 +59,33 @@ export default function Login() {
             onChange={(e) => handleInputChange(e, 'password')}
           />
 
+          {action !== 'login' ? (
+            <>
+              <FormField
+                htmlFor="firstName"
+                label="First Name"
+                value={formData.firstName}
+                onChange={(e) => handleInputChange(e, 'firstName')}
+              />
+
+              <FormField
+                htmlFor="lastName"
+                label="Last Name"
+                value={formData.lastName}
+                onChange={(e) => handleInputChange(e, 'lastName')}
+              />
+            </>
+          ) : null}
+
           <div className="w-full text-center">
-            <input
+            <button
               type="submit"
+              name="_action"
+              value={action}
               className="rounded-xl mt-2 bg-yellow-300 px-3 py-2 text-blue-600 font-semibold transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
-              value="Sign In"
-            />
+            >
+              {action === 'login' ? 'Sign In' : 'Sign Up'}
+            </button>
           </div>
         </form>
       </div>
