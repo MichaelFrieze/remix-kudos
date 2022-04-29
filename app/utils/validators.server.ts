c2a60f6989a8f20a0ad9e330a242938557e04d58
Michael Frieze <michaelfrieze@icloud.com>
2022-04-29 14:21:37 -0400

    fixed some things in validators util

--- app/utils/validators.server.ts
+++ app/utils/validators.server.ts
@@ -3,18 +3,16 @@ export const validateEmail = (email: string): string | undefined => {
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
   if (!email.length || !validRegex.test(email)) {
-    return 'Please enter a valid email address';
+    return `Please enter a valid email address`;
   }
 };
 
 export const validatePassword = (password: string): string | undefined => {
   if (password.length < 5) {
-    return 'Please enter a password with at least 5 characters';
+    return `Please enter a password that is at least 5 characters long`;
   }
 };
 
 export const validateName = (name: string): string | undefined => {
-  if (!name.length) {
-    return 'Please enter a value';
-  }
+  if (!name.length) return `Please enter a value`;
 };
