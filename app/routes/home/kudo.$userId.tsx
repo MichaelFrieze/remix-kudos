82b8e529ae690a480de4683c5ba4a3b2e28ce1c4
Michael Frieze <michaelfrieze@icloud.com>
2022-05-01 11:13:58 -0400

    using the loader function to get user data in the dynamic KudoModal route

--- app/routes/home/kudo.$userId.tsx
+++ app/routes/home/kudo.$userId.tsx
@@ -1,9 +1,27 @@
+import { json, redirect } from '@remix-run/node';
+import type { LoaderFunction } from '@remix-run/node';
 import { Modal } from '~/components/modal';
+import { getUserById } from '~/utils/user.server';
+import { useLoaderData } from '@remix-run/react';
+
+export const loader: LoaderFunction = async ({ request, params }) => {
+  const { userId } = params;
+  if (typeof userId !== 'string') {
+    return redirect('/home');
+  }
+
+  const user = await getUserById(userId);
+
+  return json({ user });
+};
 
 export default function KudoModal() {
+  const { user } = useLoaderData();
+
   return (
     <Modal isOpen={true}>
       <h2>This is a modal</h2>
+      <h2>{user.profile.firstName}</h2>
     </Modal>
   );
 }
