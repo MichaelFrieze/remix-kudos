4c55f06ccd42681541a2bff897229e848bd4ac70
Michael Frieze <michaelfrieze@icloud.com>
2022-05-01 15:56:58 -0400

    created constants utils

---
+++ app/utils/constants.ts
@@ -0,0 +1,43 @@
+export const colorMap = {
+  RED: "text-red-400",
+  GREEN: "text-green-400",
+  BLUE: "text-blue-400",
+  WHITE: "text-white",
+  YELLOW: "text-yellow-300",
+};
+
+export const backgroundColorMap = {
+  RED: "bg-red-400",
+  GREEN: "bg-green-400",
+  BLUE: "bg-blue-400",
+  WHITE: "bg-white",
+  YELLOW: "bg-yellow-300",
+};
+
+export const emojiMap = {
+  THUMBSUP: "👍",
+  PARTY: "🎉",
+  HANDSUP: "🙌🏻",
+};
+
+export const sortOptions = [
+  {
+    name: "Date",
+    value: "date",
+  },
+  {
+    name: "Sender Name",
+    value: "sender",
+  },
+  {
+    name: "Emoji",
+    value: "emoji",
+  },
+];
+
+export const departments = [
+  { name: "HR", value: "HR" },
+  { name: "Engineering", value: "ENGINEERING" },
+  { name: "Sales", value: "SALES" },
+  { name: "Marketing", value: "MARKETING" },
+];
