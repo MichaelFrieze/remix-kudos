7aeefd325e6d00a7b869df4c8a4858352536d7a8
Michael Frieze <michaelfrieze@icloud.com>
2022-05-02 15:28:27 -0400

    implemented recent recieved Kudo in the home route

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -5,22 +5,44 @@ import { requireUserId } from '~/utils/auth.server';
 import { Layout } from '~/components/layout';
 import { UserPanel } from '~/components/user-panel';
 import { getOtherUsers } from '~/utils/user.server';
+import { getFilteredKudos } from '~/utils/kudos.server';
+import { Kudo } from '~/components/kudo';
+import type { Profile, Kudo as IKudo } from '@prisma/client';
+
+interface KudoWithAuthor extends IKudo {
+  author: {
+    profile: Profile;
+  };
+}
 
 export const loader: LoaderFunction = async ({ request }) => {
   const userId = await requireUserId(request);
   const users = await getOtherUsers(userId);
 
-  return json({ users });
+  const kudos = await getFilteredKudos(userId, {}, {});
+
+  return json({ users, kudos });
 };
 
 export default function Home() {
-  const { users } = useLoaderData();
+  const { users, kudos } = useLoaderData();
 
   return (
     <Layout>
       <Outlet />
       <div className="h-full flex">
         <UserPanel users={users} />
+        <div className="flex-1 flex flex-col">
+          {/* search bar */}
+          <div className="flex-1 flex">
+            <div className="w-full p-10 flex flex-col gap-y-4">
+              {kudos.map((kudo: KudoWithAuthor) => (
+                <Kudo key={kudo.id} kudo={kudo} profile={kudo.author.profile} />
+              ))}
+            </div>
+            {/* recent kudo's */}
+          </div>
+        </div>
       </div>
     </Layout>
   );
