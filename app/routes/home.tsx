696e4a9261accace91404ce0ffabbca917e12c41
Michael Frieze <michaelfrieze@icloud.com>
2022-05-02 17:12:16 -0400

    added search-bar component to home route and implemented the search feature in the loader function

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,14 @@
-import { json } from '@remix-run/node';
 import type { LoaderFunction } from '@remix-run/node';
-import { useLoaderData, Outlet } from '@remix-run/react';
-import { requireUserId } from '~/utils/auth.server';
+import { json } from '@remix-run/node';
+import { getOtherUsers } from '~/utils/user.server';
+import { requireUserId, getUser } from '~/utils/auth.server';
 import { Layout } from '~/components/layout';
 import { UserPanel } from '~/components/user-panel';
-import { getOtherUsers } from '~/utils/user.server';
-import { getFilteredKudos, getRecentKudos } from '~/utils/kudos.server';
+import { useLoaderData, Outlet } from '@remix-run/react';
 import { Kudo } from '~/components/kudo';
-import type { Profile, Kudo as IKudo } from '@prisma/client';
+import type { Kudo as IKudo, Profile, Prisma } from '@prisma/client';
+import { SearchBar } from '~/components/search-bar';
+import { getFilteredKudos, getRecentKudos } from '~/utils/kudos.server';
 import { RecentBar } from '~/components/recent-bar';
 
 interface KudoWithAuthor extends IKudo {
@@ -20,23 +21,79 @@ export const loader: LoaderFunction = async ({ request }) => {
   const userId = await requireUserId(request);
   const users = await getOtherUsers(userId);
 
-  const kudos = await getFilteredKudos(userId, {}, {});
+  // Pull out our search & sort criteria
+  const url = new URL(request.url);
+  const sort = url.searchParams.get('sort');
+  const filter = url.searchParams.get('filter');
+  let sortOptions: Prisma.KudoOrderByWithRelationInput = {};
+  if (sort) {
+    if (sort === 'date') {
+      sortOptions = {
+        createdAt: 'desc',
+      };
+    }
+    if (sort === 'sender') {
+      sortOptions = {
+        author: {
+          profile: {
+            firstName: 'asc',
+          },
+        },
+      };
+    }
+    if (sort === 'emoji') {
+      sortOptions = {
+        style: {
+          emoji: 'asc',
+        },
+      };
+    }
+  }
 
+  let textFilter: Prisma.KudoWhereInput = {};
+  if (filter) {
+    textFilter = {
+      OR: [
+        {
+          message: {
+            mode: 'insensitive',
+            contains: filter,
+          },
+        },
+        {
+          author: {
+            OR: [
+              {
+                profile: {
+                  is: { firstName: { mode: 'insensitive', contains: filter } },
+                },
+              },
+              {
+                profile: {
+                  is: { lastName: { mode: 'insensitive', contains: filter } },
+                },
+              },
+            ],
+          },
+        },
+      ],
+    };
+  }
+  const kudos = await getFilteredKudos(userId, sortOptions, textFilter);
   const recentKudos = await getRecentKudos();
-
-  return json({ users, kudos, recentKudos });
+  const user = await getUser(request);
+  return json({ user, users, kudos, recentKudos });
 };
 
 export default function Home() {
-  const { users, kudos, recentKudos } = useLoaderData();
-
+  const { users, kudos, recentKudos, user } = useLoaderData();
   return (
     <Layout>
       <Outlet />
       <div className="h-full flex">
         <UserPanel users={users} />
         <div className="flex-1 flex flex-col">
-          {/* search bar */}
+          <SearchBar profile={user.profile} />
           <div className="flex-1 flex">
             <div className="w-full p-10 flex flex-col gap-y-4">
               {kudos.map((kudo: KudoWithAuthor) => (
